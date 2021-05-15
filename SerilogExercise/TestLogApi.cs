b8199110804db662e5fb3faf97c4929f39aac8c8
Austin Delaney <au.delaney@gmail.com>
2021-05-15 17:47:48 -0500

    Init

---
+++ SerilogExercise/TestLogApi.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerilogExercise
+{
+    public class TestLogApi
+    {
+        private readonly ILogger<TestLogApi> _logger;
+        public TestLogApi(ILogger<TestLogApi> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task DumpLogs(HttpContext context)
+        {
+            _logger.LogInformation("This is a log with INFORMATION severity level.");
+            _logger.LogWarning("This is a log with WARNING severity level.");
+            _logger.LogError("This is a log with ERROR severity level.");
+            _logger.LogCritical("This is a log with CRITICAL severity level.");
+
+            await context.Response.WriteAsync("Events has been logged.");
+        }
+    }
+}
